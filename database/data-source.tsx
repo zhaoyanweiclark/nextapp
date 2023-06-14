feabc935a7b4ea52fa7551f8a755a96e59d57af2
赵延伟 <zhaoyanweiClark@icloud.com>
2023-06-14 17:21:38 +0800

    update db

--- database/data-source.tsx
+++ database/data-source.tsx
@@ -2,32 +2,62 @@ import "reflect-metadata"
 import { DataSource, DataSourceOptions } from "typeorm"
 import { User } from "@/database/entities/user-entity"
 
-const dataSource: DataSourceOptions = {
-  type: "postgres",
-  host: process.env.POSTGRES_HOST,
-  port: 5432,
-  username: process.env.POSTGRES_USER,
-  password: process.env.POSTGRES_PASSWORD,
-  database: process.env.POSTGRES_DB,
-  synchronize: true,
-  logging: false,
-  entities: [
-    User
-  ],
-  subscribers: [],
-  migrations: [],
+let appWriteDataSource: DataSource
+export async function getAppWriteDataSource(): Promise<DataSource> {
+  if (appWriteDataSource) {
+    return appWriteDataSource
+  }
+  const writeDataSource: DataSourceOptions = {
+    type: "postgres",
+    host: process.env.POSTGRES_HOST_WRITE,
+    port: 5432,
+    username: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASSWORD,
+    database: process.env.POSTGRES_DB,
+    synchronize: true,
+    logging: false,
+    entities: [
+      User
+    ],
+    subscribers: [],
+    migrations: [],
+  }
+  appWriteDataSource = new DataSource(writeDataSource)
+  try {
+    await appWriteDataSource.initialize()
+    console.log("db write connected!")
+    return appWriteDataSource
+  } catch (err) {
+    console.error(err)
+    throw err
+  }
 }
 
-let AppDataSource: DataSource
-export async function getAppDataSource(): Promise<DataSource> {
-  if (AppDataSource) {
-    return AppDataSource
+let appReadDataSource: DataSource
+export async function getAppReadDataSource(): Promise<DataSource> {
+  if (appReadDataSource) {
+    return appReadDataSource
   }
-  AppDataSource = new DataSource(dataSource)
+  const readDataSource: DataSourceOptions = {
+    type: "postgres",
+    host: process.env.POSTGRES_HOST_READ,
+    port: 5432,
+    username: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASSWORD,
+    database: process.env.POSTGRES_DB,
+    synchronize: true,
+    logging: false,
+    entities: [
+      User
+    ],
+    subscribers: [],
+    migrations: [],
+  }
+  appReadDataSource = new DataSource(readDataSource)
   try {
-    await AppDataSource.initialize()
-    console.log("db connected!")
-    return AppDataSource
+    await appReadDataSource.initialize()
+    console.log("db read connected!")
+    return appReadDataSource
   } catch (err) {
     console.error(err)
     throw err
--- database/repositories/user-repository.tsx
+++ database/repositories/user-repository.tsx
@@ -1,9 +1,9 @@
-import { getAppDataSource } from "@/database/data-source"
+import { getAppWriteDataSource, getAppReadDataSource } from "@/database/data-source"
 import { User } from "@/database/entities/user-entity"
 import { DataSource } from "typeorm"
 
 export const allUsers = async () => {
-  const AppDataSource = await getAppDataSource()
+  const AppDataSource = await getAppReadDataSource()
   const userRepository = AppDataSource.getRepository(User)
   return userRepository.find()
 }
@@ -13,13 +13,13 @@ export const saveUser = async () => {
   user.firstName = "Timber"
   user.lastName = "Saw"
   user.age = 25
-  const AppDataSource = await getAppDataSource()
+  const AppDataSource = await getAppWriteDataSource()
   const userRepository = AppDataSource.getRepository(User)
   await userRepository.save(user)
 }
 
 export const updateUser = async (id: number) => {
-  const AppDataSource = await getAppDataSource()
+  const AppDataSource = await getAppWriteDataSource()
   const userRepository = AppDataSource.getRepository(User)
   const userToUpdate = await userRepository.findOneBy({ id })
   if (!userToUpdate) {
@@ -30,7 +30,7 @@ export const updateUser = async (id: number) => {
 }
 
 export const deleteUser = async (id: number) => {
-  const AppDataSource = await getAppDataSource()
+  const AppDataSource = await getAppWriteDataSource()
   const userRepository = AppDataSource.getRepository(User)
   const userToDelete = await userRepository.findOneBy({ id })
   if (!userToDelete) {
